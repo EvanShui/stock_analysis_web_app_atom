25ce5a1775aab9c0d6991a609e5ac20757726041
unknown <evanshui0@gmail.com>
2017-09-08 04:57:51 -0700

    working implementation of sending javascript data through ajax, processing the data with python, sending it back to javascript, and displaying the processed results

--- implementing-news-column/ajax-flask-beautifulsoup.py
+++ implementing-news-column/ajax-flask-beautifulsoup.py
@@ -1,15 +1,18 @@
 import json
-
+from bokeh.models import CustomJS, Div, Button
 from flask import Flask, jsonify, request
 from jinja2 import Template
 from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource, CustomJS, Select
 from bokeh.embed import components
 from bokeh.resources import INLINE
-from bokeh.layouts import column
+from bokeh.layouts import column, row
 from bokeh.util.string import encode_utf8
 from bokeh import events
 import math
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+import urllib.request
 
 app = Flask(__name__)
 
@@ -20,6 +23,17 @@ MY_DATABASE = {
     'bar': [i**2 for i in range(N_DATAPOINTS)],
     'baz': [i**3 for i in range(N_DATAPOINTS)]}
 
+def web_scraper(arg):
+    lst = []
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    url = "https://www.google.com/search?source=hp&q=" + str(int(arg))
+    print(url)
+    page = opener.open(url)
+    soup = BeautifulSoup(page, "html.parser")
+    for x in soup.findAll(class_="r"):
+        lst.append(x.a.contents[0].encode("utf-8"))
+    return lst
 
 @app.route("/get_new_data", methods=['POST'])
 def get_new_data():
@@ -37,9 +51,12 @@ def get_coord():
     app.logger.info(
         "Browser sent the following via AJAX: %s", json.dumps(request.form))
     variable_to_return = float(request.form['x_coord'])
+    list_to_return = web_scraper(variable_to_return)
     app.logger.info(
         "x_coord %r", (variable_to_return))
-    return jsonify({variable_to_return: (variable_to_return) + 5})
+    app.logger.info(
+        "list %r",(list_to_return))
+    return jsonify({variable_to_return: list_to_return})
 
 SIMPLE_HTML_TEMPLATE = Template('''
 <!DOCTYPE html>
@@ -63,10 +80,15 @@ def simple():
     y = MY_DATABASE[DEFAULT_VARIABLE]
 
     source = ColumnDataSource(data=dict(x=x, y=y))
+    div = Div(text="""
+    <h1> Hello </h1>
+    <h2> Good </h2>
+    <h3> Bye </h3>
+    """, width=200, height=100)
 
     plot = figure(title="Flask + JQuery AJAX in Bokeh CustomJS")
     plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
-    tap_callback = CustomJS(code="""
+    tap_callback = CustomJS(args=dict(div=div),code="""
     var x_coordinate = cb_obj['x']
     jQuery.ajax({
         type: 'POST',
@@ -75,6 +97,15 @@ def simple():
         dataType: 'json',
         success: function (json_from_server) {
             alert(JSON.stringify(json_from_server));
+            console.log(json_from_server[x_coordinate][0])
+            list = json_from_server[x_coordinate]
+            for(var i =0; i < list.length; i++){
+                var line = "<p>" + list[i] + "</p>\\n"
+                var text = div.text.concat(line)
+                var lines = text.split("\\n")
+                div.text = lines.join("\\n")
+            }
+
         },
         error: function() {
             alert("Oh no, something went wrong. Search for an error " +
@@ -109,14 +140,13 @@ def simple():
                     options=list(MY_DATABASE.keys()),
                     callback=callback)
 
-    layout = column(select, plot)
+    layout = column(select, row(plot, div))
     script, div = components(layout)
     html = SIMPLE_HTML_TEMPLATE.render(
         plot_script=script,
         plot_div=div,
         js_resources=INLINE.render_js(),
         css_resources=INLINE.render_css())
-
     return encode_utf8(html)
 
 app.run(debug=True, host="127.0.0.1", port=5002)
