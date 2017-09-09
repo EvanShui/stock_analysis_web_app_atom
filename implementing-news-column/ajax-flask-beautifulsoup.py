f2597c7371919bb23b7e83b9a54663e2961a0c49
unknown <evanshui0@gmail.com>
2017-09-09 16:35:28 -0700

    success

--- implementing-news-column/ajax-flask-beautifulsoup.py
+++ implementing-news-column/ajax-flask-beautifulsoup.py
@@ -1,5 +1,8 @@
 #importing libraries
 import json
+import datetime
+from dateutil.relativedelta import *
+from datetime import date, timedelta
 from bokeh.models import CustomJS, Div, Button
 from flask import Flask, jsonify, request
 from jinja2 import Template
@@ -7,20 +10,24 @@ from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource, CustomJS, Select
 from bokeh.embed import components
 from bokeh.resources import INLINE
-from bokeh.layouts import column, row
+from bokeh.layouts import column, row, widgetbox
 from bokeh.util.string import encode_utf8
 from bokeh import events
 import math
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import urllib.request
+import re
 
 #creating the flaks app
 app = Flask(__name__)
-
-
 #generating data for bokeh plot
 N_DATAPOINTS = 20
+
+base = date.today() + relativedelta(years=-1)
+date_list = [base - datetime.timedelta(days=x) for x in range(0, N_DATAPOINTS)]
+
+
 #the y-value points will correspond to the values associated with the 'bar' key
 DEFAULT_VARIABLE = 'bar'
 #dictionary for the y-coordinate values for bokeh plot
@@ -33,21 +40,32 @@ MY_DATABASE = {
 # Hard coded to specifically scrape the google website and returns a list of
 # the website titles from the google search results given a string to initate
 # the search query
-def web_scraper(arg):
+def web_scraper(day, month, year):
     lst = []
     opener = urllib.request.build_opener()
     #use Mozilla because can't access chrome due to insufficient privileges
-    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    #only use for google
+    #opener.addheaders = [('User-agent', 'Mozilla/5.0')]
     #url for search query
-    url = "https://www.google.com/search?source=hp&q=" + str(int(arg))
+    url = "http://www.marketwatch.com/search?q=ATVI&m=Ticker&rpp=15&mp=2005&bd=true&bd=false&bdv=" + str(month) + "%2F" + str(day) + "%2F20" + str(year) + "&rs=true"
     page = opener.open(url)
     soup = BeautifulSoup(page, "html.parser")
     #use beauitful soup to find all divs with the r class, which essentially
     #is the same as finding all of the divs that contain each individiaul search
-    #result
-    for x in soup.findAll(class_="r"):
+
+    soup_tuple_list = zip(soup.findAll(class_="searchresult"), soup.findAll(class_="deemphasized")[1:-1])
+    #iterating through a tags which includes title and links
+    #for x in soup.findAll(class_="searchresult"):
     #appends the title of each individual search result to a list
-        lst.append(x.a.contents[0].encode("utf-8"))
+    #    lst.append(x.a.encode("utf-8"))
+    #iterating through time published and publishing company
+    #gets rid of the prev strings at the beginning and end of the resulting list
+    for article, date in soup_tuple_list:
+        time = date.contents[1][5:]
+        time = re.findall(r'\|.[A-Za-z ]*', time)[0]
+        info = date.contents[0].string + time
+        article.a['target']="_blank"
+        lst.append((article.a.encode("utf-8"),info))
     return lst
 
 #generating URL for flask planning on using ajax.post method to retrieve and send back data
@@ -75,12 +93,17 @@ def get_coord():
         "Browser sent the following via AJAX: %s", json.dumps(request.form))
     #the data retrieved is in the form of a string, turn it into float to perform arithmetic operations
     variable_to_return = float(request.form['x_coord'])
+    day = request.form['day']
+    month = request.form['month']
+    year = request.form['year']
     #creates the list of data given the x-coordinate of the mouse and assigns the resulting list
-    list_to_return = web_scraper(variable_to_return)
+    list_to_return = web_scraper(day, month, year)
     app.logger.info(
         "x_coord %r", (variable_to_return))
     app.logger.info(
-        "list %r",(list_to_return))
+        "date %d %d %d", (day, month, year))
+    #app.logger.info(
+    #    "list %r",(list_to_return))
     #returns a list in form of json
     return jsonify({variable_to_return: list_to_return})
 
@@ -92,6 +115,11 @@ SIMPLE_HTML_TEMPLATE = Template('''
         <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
         {{ js_resources }}
         {{ css_resources }}
+        <style>
+            .scroll-box {
+                overflow:auto;
+            }
+        </style>
     </head>
     <body>
     {{ plot_div }}
@@ -104,45 +132,49 @@ SIMPLE_HTML_TEMPLATE = Template('''
 @app.route("/")
 #essentially the main function of this program to create bokeh diagrams
 def simple():
-    x = range(N_DATAPOINTS)
+    x = date_list
     y = MY_DATABASE[DEFAULT_VARIABLE]
 
     source = ColumnDataSource(data=dict(x=x, y=y))
 
     #the div object that will be injected into the main html template, the
     #titles of the search results will be added here
-    div = Div(text="""
-    <h1> Hello </h1>
-    <h2> Good </h2>
-    <h3> Bye </h3>
-    """, width=200, height=100)
-
+    div = Div(text="""Your <a href="https://en.wikipedia.org/wiki/HTML">HTML</a>-supported text is initialized with the <b>text</b> argument.  The
+remaining div arguments are <b>width</b> and <b>height</b>. For this example, those values
+are <i>200</i> and <i>100</i> respectively.""", width=500, height=500)
+    div.css_classes = ["scroll-box"]
     #creating the figure object
-    plot = figure(title="Flask + JQuery AJAX in Bokeh CustomJS")
+    plot = figure(x_axis_type='datetime',title="Flask + JQuery AJAX in Bokeh CustomJS")
     plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
     #callback to be executed once mouse clicks on bokeh plot
     #passing the div object into CustomJS for manipulation
     tap_callback = CustomJS(args=dict(div=div),code="""
     var x_coordinate = cb_obj['x']
+    var myDate = new Date(Math.trunc(cb_obj['x']));
+    var year = myDate.getYear() - 100;
+    var month = myDate.getMonth() + 1;
+    var day = myDate.getDate() + 1;
     jQuery.ajax({
         type: 'POST',
         url: '/get_coord',
-        data: {"x_coord": x_coordinate},
+        data: {"x_coord": x_coordinate, "day":day, "month":month,"year":year},
         dataType: 'json',
         success: function (json_from_server) {
-            alert(JSON.stringify(json_from_server));
-            console.log(json_from_server[x_coordinate][0])
+            //console.log(JSON.stringify(json_from_server));
+            //console.log(json_from_server[x_coordinate])
             //assigns the list that was sent from the flask route /get_new_data
-            list = json_from_server[x_coordinate]
+            div.text = ""
+            var list = json_from_server[x_coordinate]
             //iterates through the list and adds each element (the search query title)
             //to div
             for(var i =0; i < list.length; i++){
-                var line = "<p>" + list[i] + "</p>\\n"
-                var text = div.text.concat(line)
-                var lines = text.split("\\n")
-                div.text = lines.join("\\n")
+                var article = list[i][0]
+                var info = list[i][1]
+                var line = "<p>" + article + "<br>" + info + "</p>"
+                var lines = div.text.concat(line)
+                div.text = lines
             }
-
+            console.log("loading")
         },
         error: function() {
             alert("Oh no, something went wrong. Search for an error " +
@@ -183,7 +215,7 @@ def simple():
                     callback=callback)
 
     #constructing the html / css / js components of the bokeh graph
-    layout = column(select, row(plot, div))
+    layout = column(select, row(plot, widgetbox(div)))
     script, div = components(layout)
     html = SIMPLE_HTML_TEMPLATE.render(
         plot_script=script,
@@ -192,6 +224,6 @@ def simple():
         css_resources=INLINE.render_css())
     #returns the rendered html template with the bokeh graph
     return encode_utf8(html)
-    
+
 #runs on local server @ port 5002
 app.run(debug=True, host="127.0.0.1", port=5002)
