79a68d3bfe5138d4dbc34151eeeda81ce52ebe74
unknown <evanshui0@gmail.com>
2017-09-09 15:24:39 -0700

    successfully scrapes marketwatch's search results and appends a list of links and article info to a list

--- implementing-news-column/web-scarper.py
+++ implementing-news-column/web-scarper.py
@@ -3,6 +3,7 @@ from urllib.request import Request, urlopen
 import urllib.request
 from bs4 import BeautifulSoup
 import json as m_json
+import re
 
 if __name__ == "__main__":
     ### Import Beautiful Soup
@@ -11,12 +12,16 @@ if __name__ == "__main__":
 
     ### Create opener with Google-friendly user agent
     opener = urllib.request.build_opener()
-
-
+    # use if you want to scrape google's search results
+    #opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 
     ### Open page & generate soup
     ### the "start" variable will be used to iterate through 10 pages.
 
+    #for google
+    #url = "https://www.google.com/search?source=hp&q=1234"
+
+    #for marketwatch
     url = "http://www.marketwatch.com/search?q=ATVI&m=Ticker&rpp=15&mp=2005&bd=true&bd=false&bdv=08%2F16%2F2017&rs=true"
 
     page = opener.open(url)
@@ -25,17 +30,35 @@ if __name__ == "__main__":
     #print(soup.prettify().encode("utf-8"))
 
     lst = []
+    #for title and link of marketwatch articles
+    #for x in soup.findAll(class_="searchresult"):
+    #    print(x.a.encode("utf-8"))
+    #for x in soup.findAll(class_="deemphasized")[1:-1]:
+    #    print(x.contents[0].string)
+    #    print(x.contents[1][5:].encode("utf-8"))
 
-    for x in soup.findAll(class_="searchresult"):
-        print((x.a.contents[0].encode("utf-8")))
-        lst.append(((x.a.contents[0].encode("utf-8"))))
-
-    for x in soup.findAll(class_="deemphasized"):
-        print(x.contents[0].encode("utf-8"))
-
-    print(lst)
+    soup_tuple_list = zip(soup.findAll(class_="searchresult"), soup.findAll(class_="deemphasized")[1:-1])
+    for article, date in soup_tuple_list:
+        time = date.contents[1][5:]
+        time = re.findall(r'\|.[A-Za-z ]*', time)[0]
+        info = date.contents[0].string + time
+        print(info)
+        article.a['target'] = "_blank"
+        lst.append((article.a.encode("utf-8"),info))
     for i in lst:
-        print(str(i))
+        print(i[0])
+        print(i[1])
+
+    #for x in soup.findAll(class_="block"):
+    #    print((x.a))
+        #print((x.a.contents[0].encode("utf-8")))
+
+    #for x in soup.findAll(class_="searchresult"):
+    #    print((x.a.contents[0].encode("utf-8")))
+    #    lst.append(((x.a.contents[0].encode("utf-8"))))
+
+    #for x in soup.findAll(class_="deemphasized"):
+    #    print(x.contents[0].encode("utf-8"))
 
     #for i in soup.findAll(class_="f"):
     #    print(i)
