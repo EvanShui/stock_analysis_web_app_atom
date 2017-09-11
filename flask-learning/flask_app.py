3f19e68c88b635fcc7b916eac0ca11731fc05961
unknown <evanshui0@gmail.com>
2017-09-11 03:32:57 -0700

    working with ajax and flask

--- flask-learning/flask_app.py
+++ flask-learning/flask_app.py
@@ -1,19 +1,40 @@
 #flask app
 from flask import Flask, render_template
 from datetime import datetime
-from BasicLine import js,div, cdn_js, cdn_css
-from bokeh.embed import autoload_server
-from bokeh.client import pull_session
+from BasicLine import js, div, cdn_js, cdn_css
+from BasicLine import web_scraper
+from flask import Flask, render_template, jsonify, request, url_for
+import json
 
 #instantiate the flask append
 app = Flask(__name__)
 
+@app.route("/update_y_data", methods=['POST'])
+def get_y_data():
+    app.logger.info(
+        "Browser sent the following via AJAX: %s", json.dumps(request.form))
+    ticker = request.form['ticker_sent']
+    app.logger.info(
+        "ticker %r", (ticker))
+    return jsonify({ticker:[10, 9, 8, 7, 6]})
+
+@app.route("/get_data",methods=['POST'])
+def get_coord():
+    app.logger.info(
+        "Browser sent the following via AJAX: %s", json.dumps(request.form))
+    #the data retrieved is in the form of a string, turn it into float to perform arithmetic operations
+    x_coordinate = float(request.form['x_coord'])
+    #creates the list of data given the x-coordinate of the mouse and assigns the resulting list
+    web_url = web_scraper(x_coordinate)
+    app.logger.info(
+        "x_coord %r", (x_coordinate))
+    #returns a list in form of json
+    return jsonify({x_coordinate: web_url})
+
 #create index page function
 @app.route("/")
 def index():
-    session=pull_session(app_path="http://localhost:5006/random_generator")
-    bokeh_script=autoload_server(None,app_path="http://localhost:5006/random_generator",session_id=session.id)
-    return render_template("index.html", js=js, div=div, cdn_js=cdn_js, cdn_css=cdn_css, current_date=datetime.now())
+    return render_template("index.html", js=js, div=div, cdn_js=cdn_js, cdn_css=cdn_css)
 
 #run the app
 if __name__ == "__main__":
