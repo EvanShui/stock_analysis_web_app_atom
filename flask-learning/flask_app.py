ed47212c1ae142c5fe1b937bba1db994c33a6c10
unknown <evanshui0@gmail.com>
2017-09-13 06:32:17 -0700

    finish implementing resize range route can use BaseLine.stock_ticker to grab stock ticker

--- flask-learning/flask_app.py
+++ flask-learning/flask_app.py
@@ -1,22 +1,43 @@
 #flask app
 from flask import Flask, render_template
 from datetime import datetime
-from BasicLine import js, div, cdn_js, cdn_css
-from BasicLine import web_scraper
+import BasicLine
+from BasicLine import js, div, cdn_js, cdn_css, dates, stock_ticker
+from BasicLine import web_scraper, data_to_CDS_y, get_data
 from flask import Flask, render_template, jsonify, request, url_for
 import json
 
 #instantiate the flask append
 app = Flask(__name__)
 
+@app.route("/resize_y_range", methods=['POST'])
+def resize_y_range():
+    app.logger.info(
+        "Browser sent the following via AJAX: %s", json.dumps(request.form))
+    date_index = request.form['index']
+    app.logger.info(
+        "Resize basicline ticker: %s", BasicLine.stock_ticker)
+    return jsonify({date_index:[5,4,3,2,1]})
+
 @app.route("/update_y_data", methods=['POST'])
 def get_y_data():
+    global stock_ticker
     app.logger.info(
         "Browser sent the following via AJAX: %s", json.dumps(request.form))
     ticker = request.form['ticker_sent']
+    test_ticker = "atvi"
     app.logger.info(
-        "ticker %r", (ticker))
-    return jsonify({ticker:[10, 9, 8, 7, 6]})
+        "Original basicline ticker: %s", BasicLine.stock_ticker)
+    BasicLine.stock_ticker = test_ticker
+    app.logger.info(
+        "New basicline ticker: %s", BasicLine.stock_ticker)
+    data, meta_data = get_data(test_ticker)
+    sources_y_list = data_to_CDS_y(data, dates[5])
+    #app.logger.info(
+    #    "ticker %r", (sources_y_list))
+    #app.logger.info(
+    #    "sources_list %r", (sources_list))
+    return jsonify({ticker:(sources_y_list[0], sources_y_list[1])})
 
 @app.route("/get_data",methods=['POST'])
 def get_coord():
