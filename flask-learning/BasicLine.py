790a2b9ace4a2639f6962591e5a73472c9a526fe
unknown <evanshui0@gmail.com>
2017-09-13 06:32:59 -0700

    done with callback for button, finish radio button group and tap then we're done

--- flask-learning/BasicLine.py
+++ flask-learning/BasicLine.py
@@ -1,7 +1,7 @@
 #Making a basic Bokeh line graph
 
 #importing Bokeh
-from pandas_datareader import data, wb
+import pandas_datareader.data as web
 import datetime
 from bokeh.plotting import figure, show, output_file, ColumnDataSource
 from bokeh.io import curdoc
@@ -28,8 +28,23 @@ import urllib.request
 import re
 from bokeh.resources import INLINE
 from bokeh.events import ButtonClick
+from alpha_vantage.timeseries import TimeSeries
+import math
+from bokeh.models.widgets import RadioButtonGroup
+import time
 
-stock_ticker = "atvi"
+resources = INLINE
+js_resources = resources.render_js()
+css_resources = resources.render_css()
+
+stock_ticker = "nflx"
+
+def get_data(stock_ticker):
+    ts = TimeSeries(key='VVKDMK4DCJUF1NQP', output_format='pandas')
+    data, meta_data = ts.get_daily(symbol=stock_ticker, outputsize='full')
+    return data,meta_data
+
+data,meta_data = get_data(stock_ticker)
 
 x=[1,2,3,4,5]
 y=[6,7,8,9,10]
@@ -40,11 +55,13 @@ delta_3_months = date.today() + relativedelta(months=-3)
 delta_6_months = date.today() + relativedelta(months=-6)
 delta_year = date.today() + relativedelta(years=-1)
 delta_5_year = date.today() + relativedelta(years=-5)
-dates = [delta_7_days, delta_month, delta_3_months, delta_6_months, delta_year, delta_5_year]
+dates = [delta_7_days, delta_month, delta_3_months, delta_6_months,delta_year, delta_5_year, date.today()]
 
-resources = INLINE
-js_resources = resources.render_js()
-css_resources = resources.render_css()
+map_ints = map(lambda date: time.mktime(date.timetuple()) * 1000, dates)
+date_ints = [date_int for date_int in map_ints]
+
+tools_lst = "pan,wheel_zoom,box_zoom,reset"
+date_titles = ["week", "month", "3 months", "6 months", "1 year", "3 years"]
 
 text_input = TextInput()
 button = Button(label="main")
@@ -52,41 +69,27 @@ button2 = Button()
 output=Paragraph()
 output.text = "goodbye"
 
-def data_to_CDS(stock_ticker, start_date):
-    df = data.DataReader(name=stock_ticker, data_source="google", start=start_date, end=date.today())
-    df['stock_ticker'] = stock_ticker
+def data_to_CDS(stock_ticker, data, start_date):
+    delta_days = np.busday_count(start_date, date.today())
+    print(delta_days)
+    data['ticker'] = stock_ticker
+    adjusted_data = data.tail(delta_days)
     source = ColumnDataSource(data=dict(
-        date=np.array(df['Close'].index, dtype=np.datetime64),
-        price=np.array(df['Close'].values),
-        ticker=np.array(df['stock_ticker'])
+        date=np.array(adjusted_data['close'].index, dtype=np.datetime64),
+        price=adjusted_data['close'].values,
+        index=adjusted_data['ticker']
     ))
     return source
 
-def plot(p, source):
-    return (p.line('date', 'price', source=source, line_width=2))
+def data_to_CDS_y(data, start_date):
+    delta_days = np.busday_count(start_date, date.today())
+    print("y",delta_days)
+    adjusted_data = data['close'].tail(delta_days)
+    return (np.array(adjusted_data.index, dtype=np.datetime64).tolist(), np.array(adjusted_data.values).tolist())
 
-source = ColumnDataSource(data=dict(x=x,y=y))
-sources_list = np.array([data_to_CDS(stock_ticker, date) for date in dates])
-source_wrapper = ColumnDataSource(data=dict(arg=sources_list))
-#print(sources_list)
-tools_lst = "pan,wheel_zoom,box_zoom,reset"
-date_titles = ["week", "month", "3 months", "6 months", "year", "5 years"]
-figures_list = [figure(width=700, height=500, tools=tools_lst,
-           title=title) for title in date_titles]
-fig_source_list = zip(figures_list, sources_list)
-for fig, source in fig_source_list:
-    plot(fig,source)
-
-fig_date_titles_list = zip(figures_list, date_titles)
-
-tab_list = [Panel(child=fig, title=date_title) for fig,date_title in fig_date_titles_list]
-#print(tab_list[0]._property_values['child']._property_values['y_range'])
-tabs = Tabs(tabs=tab_list)
-print(tabs._property_values['tabs'][0]._property_values['child'])
-
-#print(figures_list)
-
-y2=[10,9,8,7,6]
+p = figure(x_axis_type="datetime", tools=tools_lst)
+source = data_to_CDS(stock_ticker, data, delta_5_year)
+p.line('date', 'price', source=source, line_width=2)
 
 def web_scraper(x_coord):
     opener = urllib.request.build_opener()
@@ -97,16 +100,9 @@ def web_scraper(x_coord):
 def button_click():
     output.text += "hello"
 
-button_callback = CustomJS(args=dict(text_input=text_input, output=output,source_wrapper=source_wrapper),code="""
+button_callback = CustomJS(args=dict(text_input=text_input, output=output, source=source),code="""
      //var plot_data = source.data;
      var ticker = text_input.value;
-     //iterate through source wrapper class, edit all of the y-values, go from there.
-     var sources_list = source_wrapper.data['arg']
-     console.log(sources_list.length)
-     for (var i = 0; i < sources_list.length; i++){
-        console.log(sources_list[i])
-     }
-
      jQuery.ajax({
         type: 'POST',
         url: '/update_y_data',
@@ -114,16 +110,19 @@ button_callback = CustomJS(args=dict(text_input=text_input, output=output,source
         dataType: 'json',
         success: function (json_from_server) {
             alert(JSON.stringify(json_from_server));
-            //output.text += ticker
-            //plot_data['y'] = json_from_server[ticker];
-            //source.trigger('change');
+            var updated_price_list = json_from_server[ticker];
+            //source.data['date'] = json_from_server[ticker][0]
+            source.data['price'] = json_from_server[ticker][1];
+            source.trigger('change');
+            var actual_ticker = %r;
+            console.log(actual_ticker)
         },
         error: function() {
             alert("Oh no, something went wrong. Search for an error " +
                   "message in Flask log and browser developer tools.");
         }
     });
-    """)
+    """ % (stock_ticker))
 
 tap_callback = CustomJS(args=dict(output=output), code="""
     var x_coordinate = cb_obj['x']
@@ -148,25 +147,41 @@ tap_callback = CustomJS(args=dict(output=output), code="""
     });
     """)
 
-button.js_on_event(ButtonClick, CustomJS(code="console.log('hello')"))
+
+radio_button_callback = CustomJS(args=dict(fig=p), code="""
+            var date_ints = %s;
+            var active_button = cb_obj.active
+            var stock_ticker = %r;
+            console.log(active_button)
+            fig.x_range.start = date_ints[active_button]
+            fig.x_range.end = date_ints[6]
+            jQuery.ajax({
+                type: 'POST',
+                url: '/resize_y_range',
+                data: {"index": active_button},
+                dataType: 'json',
+                success: function (json_from_server) {
+                    var test = json_from_server[active_button]
+                    console.log("loading")
+                    console.log(test)
+                },
+                error: function() {
+                    alert("Oh no, something went wrong. Search for an error " +
+                          "message in Flask log and browser developer tools.");
+                }
+            });
+        """ % (date_ints, stock_ticker))
+
 button2.js_on_event(ButtonClick, button_callback)
-#prepare some data
 
-
-#create a figure object
-f=figure()
-
-#create line plot
-f.line('x','y',source=source)
-f.js_on_event('tap', tap_callback)
-
-lay_out = column(row(column(text_input,output), column(button,button2)), tabs)
+radio_button_group = RadioButtonGroup(
+        labels=["1w", "1m", "3m", "6m", "1y", "5y"], active=5, callback=radio_button_callback)
+lay_out = column(radio_button_group, row(text_input, button2), p)
 
 js,div=components(lay_out, INLINE)
 
 cdn_js=INLINE.render_js()
 cdn_css=INLINE.render_css()
-
 
 #write the plot in the figure object
 #show(f)
